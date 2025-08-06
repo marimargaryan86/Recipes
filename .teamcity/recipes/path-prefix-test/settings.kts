6ecd109149fce41d731ceb31674da553a2ca598c
marimargaryan86 <35734002+marimargaryan86@users.noreply.github.com>
2025-08-06 14:57:20 +0400

    Update settings.kts

--- .teamcity/recipes/path-prefix-test/settings.kts
+++ .teamcity/recipes/path-prefix-test/settings.kts
@@ -1,1 +1,21 @@
 
+version = "2023.11"
+
+project {
+    buildType {
+        id("PathPrefixFail")
+        name = "Exec Mode Fails With TEAMCITY_PATH_PREFIX"
+
+        params {
+            param("env.TEAMCITY_PATH_PREFIX", "/opt/custom-bin")
+        }
+
+        steps {
+            exec {
+                name = "Run mytool using exec (should fail)"
+                path = "mytool"
+                arguments = ""
+            }
+        }
+    }
+}
