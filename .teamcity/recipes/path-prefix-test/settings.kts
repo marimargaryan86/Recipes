972333fcf866d4610f5fc92ac75e6d11bb7e527d
marimargaryan86 <35734002+marimargaryan86@users.noreply.github.com>
2025-08-06 14:58:19 +0400

    Update settings.kts

--- .teamcity/recipes/path-prefix-test/settings.kts
+++ .teamcity/recipes/path-prefix-test/settings.kts
@@ -2,6 +2,7 @@
 version = "2023.11"
 
 project {
+    applyRecipe("path-prefix-test", version = "1.0")
     buildType {
         id("PathPrefixFail")
         name = "Exec Mode Fails With TEAMCITY_PATH_PREFIX"
